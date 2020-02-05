b878aec9ef0d49fdc857765225e529188b2687ff
Marcelo Barsotti <mbarsott@gmail.com>
2020-02-04 20:55:12 -0500

    4.2 Application Menu

---
+++ menu-demo/src/main.js
@@ -0,0 +1,36 @@
+const electron = require("electron");
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
+
+app.on("ready", _ => {
+  new BrowserWindow();
+  const name = electron.app.getName();
+  const template = [
+    {
+      label: name,
+      submenu: [
+        {
+          label: `About ${name}`,
+          click: _ => {
+            console.log("clicked about");
+          },
+          role: "about"
+        },
+        {
+          type: "separator"
+        },
+        {
+          label: "Quit",
+          click: _ => {
+            app.quit();
+          },
+          accelerator: "Cmd+Q"
+        }
+      ]
+    }
+  ];
+  menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+});
